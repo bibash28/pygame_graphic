aedf18c0dab9a5c020674631f5521f200da397cb
bibash28 <bibashshrestha@gmail.com>
2018-07-26 09:13:55 +0545

    Update 3Dtranslate.py

--- 3Dtranslate.py
+++ 3Dtranslate.py
@@ -3,7 +3,7 @@ pygame.init()
 from math import sin, cos, radians
 
 window = pygame.display.set_mode((640,480))
-pygame.display.set_caption("Homogeneous Transformation")
+pygame.display.set_caption("#D Translation")
 clock = pygame.time.Clock()
 
 black = (0,0,0)
